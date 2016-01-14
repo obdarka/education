89ecef5ab8220f373577e0f9c496838f8c87401f
dariakovalenko <dariakovalenko@gmail.com>
2016-01-14 23:05:35 +0200

    Initial Commit

---
+++ DKKit/DKKit.h
@@ -0,0 +1,19 @@
+//
+//  DKKit.h
+//  DKKit
+//
+//  Created by Daria on 14.01.16.
+//  Copyright © 2016 Daria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKKit.
+FOUNDATION_EXPORT double DKKitVersionNumber;
+
+//! Project version string for DKKit.
+FOUNDATION_EXPORT const unsigned char DKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKKit/PublicHeader.h>
+
+
